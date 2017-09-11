07fd447467ed8777f8a904775e73b53edb1276b0
Sajid Rabbani <sajid@nascenia.com>
2017-09-11 18:16:54 +0600

    Added test for user signup

---
+++ spec/features/signup_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature 'Signup Users' do
+
+  scenario 'with valid credentials' do
+    visit '/'
+    click_link 'Sign up'
+    fill_in 'Email', with: 'joe@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password Confirmation', with: 'password'
+    click_button 'Sign up'
+
+    expect(page).to have_content('You have signed up successfully')
+  end
+end
