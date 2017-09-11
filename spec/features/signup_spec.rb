a2a222fd4ab92e90d56eceaf1ec4217696bb824c
Sajid Rabbani <sajid@nascenia.com>
2017-09-11 19:27:58 +0600

    Implemented sign up functionality

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature 'Signup Users' do
     click_link 'Sign up'
     fill_in 'Email', with: 'joe@example.com'
     fill_in 'Password', with: 'password'
-    fill_in 'Password Confirmation', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
     click_button 'Sign up'
 
     expect(page).to have_content('You have signed up successfully')
