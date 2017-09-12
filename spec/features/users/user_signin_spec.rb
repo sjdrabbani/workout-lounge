5bb0ca128560fa61164c061915ede2e039cbef03
Sajid Rabbani <sajid@nascenia.com>
2017-09-12 10:13:32 +0600

    Implemented sign in feature

--- spec/features/users/user_signin_spec.rb
+++ spec/features/users/user_signin_spec.rb
@@ -13,8 +13,8 @@ RSpec.feature 'User sign in' do
     fill_in 'Password', with: @john.password
     click_button 'Log in'
 
-    expect(page).to have_content('Signed up successfully')
-    expect(page).to have_content("Signed up as #{john.email}")
+    expect(page).to have_content('Signed in successfully')
+    expect(page).to have_content("Signed in as #{@john.email}")
   end
 end
 
