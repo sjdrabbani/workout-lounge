4615f9e6d2137a2715f229fff27566cfe20a3e04
Sajid Rabbani <sajid@nascenia.com>
2017-09-15 12:24:06 +0600

    Implement exercise creation & add name to user
    Add first_name & last_name of users
    Add full_name method in user model
    Add create method for exercise
    Implement new & form page of creating exercise
    Modified spec of creating_exercise

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -14,8 +14,8 @@ RSpec.feature 'Creating exercise' do
     expect(page).to have_link('Back')
 
     fill_in 'Duration', with: 70
-    fill_in 'Workout Details', with: 'Pushups'
-    fill_in 'Activity Date', with: '2017-09-14'
+    fill_in 'Workout details', with: 'Pushups'
+    fill_in 'Activity date', with: '2017-09-14'
     click_button 'Create Exercise'
 
     expect(page).to have_content('Exercise has been created.')
