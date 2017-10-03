ef0b817203d3b8b4ec17d9f8b53170158f2b2e55
Sajid Rabbani <sajid@nascenia.com>
2017-10-03 13:17:23 +0600

    Add test for deleting an exercise

---
+++ spec/features/exercises/deleting_exercise_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'Deleting exercise' do
+
+  before do
+    @john = User.create(email: 'john@example.com', password: 'password')
+
+    @exercise_one = @john.exercises.create(duration_in_min: 45,
+                                           workout: 'Pushup',
+                                           workout_date: Date.today)
+
+    login_as(@john)
+  end
+
+  scenario  do
+    visit '/'
+    click_link 'My Lounge'
+
+    path = "/users/#{@john.id}/exercises/#{@exercise_one.id}"
+    link = "//a[contains(@href, '#{path}') and .//text()='Destroy']"
+    find(link).click
+
+    expect(page).to have_content('Exercise has been deleted')
+  end
+end
