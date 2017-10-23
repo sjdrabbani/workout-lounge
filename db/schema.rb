b845d795f962ec649473afa5de417f37dd56223a
Sajid Rabbani <sajid@nascenia.com>
2017-10-23 09:47:44 +0600

    Implement form for chatting
    - Implement form for chatting in the chat window with a room name
    - Create message model
    - Run migration form message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023030601) do
+ActiveRecord::Schema.define(version: 20171023034524) do
 
   create_table "exercises", force: :cascade do |t|
     t.integer "duration_in_min"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20171023030601) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "rooms", force: :cascade do |t|
